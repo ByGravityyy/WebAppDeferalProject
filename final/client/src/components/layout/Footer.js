b0350382d6abaaa7958971618f4671b9f5d0c736
ByGravityyy <39156359+ByGravityyy@users.noreply.github.com>
2021-08-24 18:25:22 +0100

    Add files via upload

---
+++ final/client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+      James Gardiner - Student ID: 19823656
+    </footer>
+  );
+};
---
+++ final/config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb://admin:hkQDKDM4mQknzC9t@clustermain-shard-00-00.xh70f.mongodb.net:27017,clustermain-shard-00-01.xh70f.mongodb.net:27017,clustermain-shard-00-02.xh70f.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-opt3pa-shard-0&authSource=admin&retryWrites=true&w=majority',
+  secretOrKey: 'secret'
+};
